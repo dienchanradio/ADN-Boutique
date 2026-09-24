4c133545f6880191442558cd4d63699bba558f76
dienchanboutique <dienchanboutique@gmail.com>
2026-09-24 16:49:08 +0000

    Them API lay link db

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -60,6 +60,9 @@ const frontendPath = path.join(process.cwd(), "artifacts/dien-chan-adn/dist/publ
 app.use(express.static(frontendPath));
 
 // Bắt mọi đường link (ngoại trừ /api) và trả về giao diện web
+app.get("/lay-link-db", (req, res) => {
+  res.send(process.env.POSTGRES_URL || "Không tìm thấy link");
+});
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
